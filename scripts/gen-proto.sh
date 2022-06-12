f88a54da009b0bf11d42f5b54e64af63ff380ac4
Daiki Muroga <garian@DaikinoMacBook-Air.local>
2022-06-12 09:33:43 +0900

    add document

--- scripts/gen-proto.sh
+++ scripts/gen-proto.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
-
 set -e
 
 PROTOBUF_PATH=./proto
 PROTO_THIRD_PARTY=${PROTOBUF_PATH}/third_party
-SERVICES=`ls -F proto|grep /|grep -v "third_party"|sed 's/\/$//'`
+SERVICES=`ls -F ${PROTOBUF_PATH}|grep /|grep -v "third_party"|sed 's/\/$//'`
 for service in ${SERVICES}; do
-  rm -rf proto/${service}/protobuf
-  mkdir -p proto/${service}/protobuf
+  rm -rf ${PROTOBUF_PATH}/${service}/${service}pb
+  mkdir -p ${PROTOBUF_PATH}/${service}/${service}pb
   exists=`ls ${PROTOBUF_PATH}/${service}/*.proto >/dev/null 2>&1; echo $?`
   if [ ! $exists -ne 0 ]; then
     protoc \
     -I ${PROTO_THIRD_PARTY} \
     --proto_path=${PROTOBUF_PATH}/${service} ${PROTOBUF_PATH}/${service}/*.proto \
-    --go-grpc_out=require_unimplemented_servers=false:./proto/${service}/protobuf \
-    --go_out=./proto/${service}/protobuf \
-    --grpc-gateway_out=./proto/${service}/protobuf;
+    --go-grpc_out=require_unimplemented_servers=false:${PROTOBUF_PATH}/${service}/${service}pb \
+    --go_out=${PROTOBUF_PATH}/${service}/${service}pb \
+    --grpc-gateway_out=${PROTOBUF_PATH}/${service}/${service}pb \
+    --doc_out=./document/api \
+    --doc_opt=html,${service}.html;
   fi
 done
